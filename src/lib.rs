68b8d1208ad42226b9814c6ed6db93b9c88ca949
Thai Nguyen <tnohtition@gmail.com>
2025-07-20 21:51:48 +0700

    Organise project

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,3 @@
-use actix_web::dev::Server;
-use actix_web::{App, HttpResponse, HttpServer, Responder, web};
-use std::net::TcpListener;
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
-
-// We need to mark `run` as public.
-// It is no longer a binary entrypoint, therefore we can mark it as async
-// without having to use any proc-macro incantation.
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
-    // .await
-
-    Ok(server)
-}
+pub mod configuration;
+pub mod routes;
+pub mod startup;
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -5,6 +5,21 @@
 // `cargo expand --test health_check` (<- name of the test file)
 use std::net::TcpListener;
 
+// Spin up an instance of our application
+// and returns its address (i.e. http://localhost:xxxx)
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+
+    let server = zero2prod::startup::run(listener).expect("Failed to bind address");
+    // Launch the server as a background task
+    // tokio::spawn returns a handle to the spawned future,
+    // but we have no use for it here, hence the non-binding let
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
+}
+
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
@@ -25,16 +40,45 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-// Launch our application in the background ~somehow~
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    let port = listener.local_addr().unwrap().port();
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
 
-    let server = zero2prod::run(listener).expect("Failed to bind address");
-    // Launch the server as a background task
-    // tokio::spawn returns a handle to the spawned future,
-    // but we have no use for it here, hence the non-binding let
-    let _ = tokio::spawn(server);
+    assert_eq!(200, response.status().as_u16());
+}
 
-    format!("http://127.0.0.1:{}", port)
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email")
+    ];
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload as {}.",
+            error_message
+        );
+    }
 }
