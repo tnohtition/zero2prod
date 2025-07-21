92de5c92f23391dd70999ea739fe62f1c338c78b
Thai Nguyen <tnohtition@gmail.com>
2025-07-21 19:42:44 +0700

    cargo clippy

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,12 +1,13 @@
 use actix_web::{HttpResponse, Responder, web};
 use serde::Deserialize;
 
+#[allow(dead_code)]
 #[derive(Deserialize)]
 pub struct FormData {
     email: String,
     name: String,
 }
 
-pub async fn subscribe(form: web::Form<FormData>) -> impl Responder {
+pub async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
     HttpResponse::Ok()
 }
